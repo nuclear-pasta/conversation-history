22b60802dcd4b2a88bb0d81f35d45cfda5c99f6a
Dario Spina <dariospina@Darios-MBP.broadband>
2023-06-15 09:18:07 +0100

    feat: create state machine for project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,28 @@
 class Project < ApplicationRecord
     has_many :comments, as: :subject, dependent: :destroy
     validates :name, :state, presence: true
+    
+    state_machine :state, initial: :draft do
+        state :draft
+        state :submitted
+        state :approved
+        state :rejected
+        state :cancelled
+    
+        event :submit do
+          transition draft: :submitted
+        end
+    
+        event :approve do
+          transition submitted: :approved
+        end
+    
+        event :reject do
+          transition submitted: :rejected
+        end
+    
+        event :cancel do
+          transition any => :cancelled
+        end
+      end
 end
